ca797db1a26eebd749b352a50ef978552bd910ca
Matthieu Coudron <mcoudron@hotmail.com>
2023-01-06 03:10:53 +0100

    chore: reformat grammar

--- grammar.js
+++ grammar.js
@@ -16,9 +16,7 @@ module.exports = grammar(json, {
 
     request: ($) =>
       seq(field("method", $.method), $._whitespace, field("url", $.url)),
-
     method: (_) => /(GET|POST|PATCH|DELETE|PUT)/,
-
     url: (_) =>
       /((www|http:|https:)\/\/(?:w{1,3}\.)?[^\s.]+(?:\.[a-z]+)*(?::\d+)?|\{\{\w+\}\})([\w.,@?^=%&amp;:\/~+#\{\}\u00C0-\u00FF-]*[\w@?^=%&amp;\/~+#\u00C0-\u00FF-])?/,
 
@@ -28,12 +26,8 @@ module.exports = grammar(json, {
         $._whitespace,
         field("value", $.value)
       ),
-
     name: (_) => /[A-Za-z-]+/,
-
     value: (_) => /[^\n]+/,
-
-    json_file: (_) => seq(/[A-Za-z-_./]+/, ".json"),
 
     external_body: ($) =>
       seq(
@@ -41,6 +35,8 @@ module.exports = grammar(json, {
         $._whitespace,
         field("json_file", $.json_file),
       ),
+    json_file: (_) => seq(/[A-Za-z-_./]+/, ".json"),
+
 
     comment: ($) => token(seq("#", /.*/)),
 
