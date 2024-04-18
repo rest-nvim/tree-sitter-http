0c1b82d1105b9adcb9c8dbc233207f17b6ff9f42
disrupted <hi@salomonpopp.me>
2024-04-18 21:40:15 +0200

    refactor(response): alternate regex

--- grammar.js
+++ grammar.js
@@ -99,7 +99,7 @@ module.exports = grammar({
                 ),
             ),
 
-        status_code: ($) => /[1-5][0-9][0-9]/,
+        status_code: ($) => /[1-5]\d{2}/,
         status: ($) => /(OK)/,
         response: ($) => seq($.http_version, $._whitespace, $.status_code, $._whitespace, $.status),
 
