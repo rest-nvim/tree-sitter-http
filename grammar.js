62f6cd167cd30a5ccd92f50c60b2b590c7280364
NTBBloodbath <NTBBloodbath@users.noreply.github.com>
2024-06-29 21:44:21 +0000

    chore: autoformat with dprint

--- grammar.js
+++ grammar.js
@@ -21,7 +21,8 @@ module.exports = grammar({
         comment: (_) => token(seq("#", /.*/)),
 
         // LIST http verb is arbitrary and required to use vaultproject
-        method: ($) => /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH|LIST)/,
+        method: ($) =>
+            /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH|LIST)/,
 
         host: ($) => seq($.identifier, optional($.port)),
         port: ($) => seq(":", /\d+/),
@@ -79,7 +80,8 @@ module.exports = grammar({
 
         target_url: ($) =>
             choice(
-                seq($.path, repeat($.query_param), optional(seq("#", $.fragment))),
+                seq($.path, repeat($.query_param),
+                    optional(seq("#", $.fragment))),
                 seq(
                     optional(seq($.scheme, "://")),
                     optional($.authority),
@@ -106,8 +108,11 @@ module.exports = grammar({
             ),
 
         status_code: ($) => /[1-5]\d{2}/,
-        status_text: ($) => /(Continue|Switching Protocols|Processing|OK|Created|Accepted|Non-Authoritative Information|No Content|Reset Content|Partial Content|Multi-Status|Already Reported|IM Used|Multiple Choices|Moved Permanently|Found|See Other|Not Modified|Use Proxy|Switch Proxy|Temporary Redirect|Permanent Redirect|Bad Request|Unauthorized|Payment Required|Forbidden|Not Found|Method Not Allowed|Not Acceptable|Proxy Authentication Required|Request Timeout|Conflict|Gone|Length Required|Precondition Failed|Payload Too Large|URI Too Long|Unsupported Media Type|Range Not Satisfiable|Expectation Failed|I'm a teapot|Misdirected Request|Unprocessable Entity|Locked|Failed Dependency|Too Early|Upgrade Required|Precondition Required|Too Many Requests|Request Header Fields Too Large|Unavailable For Legal Reasons|Internal Server Error|Not Implemented|Bad Gateway|Service Unavailable|Gateway Timeout|HTTP Version Not Supported|Variant Also Negotiates|Insufficient Storage|Loop Detected|Not Extended|Network Authentication Required)/,
-        response: ($) => seq($.http_version, $._whitespace, $.status_code, $._whitespace, $.status_text),
+        status_text: ($) =>
+            /(Continue|Switching Protocols|Processing|OK|Created|Accepted|Non-Authoritative Information|No Content|Reset Content|Partial Content|Multi-Status|Already Reported|IM Used|Multiple Choices|Moved Permanently|Found|See Other|Not Modified|Use Proxy|Switch Proxy|Temporary Redirect|Permanent Redirect|Bad Request|Unauthorized|Payment Required|Forbidden|Not Found|Method Not Allowed|Not Acceptable|Proxy Authentication Required|Request Timeout|Conflict|Gone|Length Required|Precondition Failed|Payload Too Large|URI Too Long|Unsupported Media Type|Range Not Satisfiable|Expectation Failed|I'm a teapot|Misdirected Request|Unprocessable Entity|Locked|Failed Dependency|Too Early|Upgrade Required|Precondition Required|Too Many Requests|Request Header Fields Too Large|Unavailable For Legal Reasons|Internal Server Error|Not Implemented|Bad Gateway|Service Unavailable|Gateway Timeout|HTTP Version Not Supported|Variant Also Negotiates|Insufficient Storage|Loop Detected|Not Extended|Network Authentication Required)/,
+        response: ($) =>
+            seq($.http_version, $._whitespace, $.status_code, $._whitespace,
+                $.status_text),
 
         request: ($) =>
             prec.right(
