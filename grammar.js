d2022f962438332ef9e3532d6bddf975080a1284
Seongmin Lee <boltlessengineer@gmail.com>
2024-07-25 16:07:30 +0000

    style: format

--- grammar.js
+++ grammar.js
@@ -28,7 +28,8 @@ module.exports = grammar({
         request_separator: (_) => seq(token(prec(1, "###")), LINE_TAIL),
 
         // LIST http verb is arbitrary and required to use vaultproject
-        method: (_) => /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH|LIST)/,
+        method: (_) =>
+            /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH|LIST)/,
 
         http_version: (_) => token(prec(0, /HTTP\/[\d\.]+/)),
 
@@ -54,8 +55,10 @@ module.exports = grammar({
             ),
 
         status_code: ($) => /[1-5]\d{2}/,
-        status_text: ($) => /(Continue|Switching Protocols|Processing|OK|Created|Accepted|Non-Authoritative Information|No Content|Reset Content|Partial Content|Multi-Status|Already Reported|IM Used|Multiple Choices|Moved Permanently|Found|See Other|Not Modified|Use Proxy|Switch Proxy|Temporary Redirect|Permanent Redirect|Bad Request|Unauthorized|Payment Required|Forbidden|Not Found|Method Not Allowed|Not Acceptable|Proxy Authentication Required|Request Timeout|Conflict|Gone|Length Required|Precondition Failed|Payload Too Large|URI Too Long|Unsupported Media Type|Range Not Satisfiable|Expectation Failed|I'm a teapot|Misdirected Request|Unprocessable Entity|Locked|Failed Dependency|Too Early|Upgrade Required|Precondition Required|Too Many Requests|Request Header Fields Too Large|Unavailable For Legal Reasons|Internal Server Error|Not Implemented|Bad Gateway|Service Unavailable|Gateway Timeout|HTTP Version Not Supported|Variant Also Negotiates|Insufficient Storage|Loop Detected|Not Extended|Network Authentication Required)/,
-        response: ($) => seq($.http_version, WS, $.status_code, WS, $.status_text),
+        status_text: ($) =>
+            /(Continue|Switching Protocols|Processing|OK|Created|Accepted|Non-Authoritative Information|No Content|Reset Content|Partial Content|Multi-Status|Already Reported|IM Used|Multiple Choices|Moved Permanently|Found|See Other|Not Modified|Use Proxy|Switch Proxy|Temporary Redirect|Permanent Redirect|Bad Request|Unauthorized|Payment Required|Forbidden|Not Found|Method Not Allowed|Not Acceptable|Proxy Authentication Required|Request Timeout|Conflict|Gone|Length Required|Precondition Failed|Payload Too Large|URI Too Long|Unsupported Media Type|Range Not Satisfiable|Expectation Failed|I'm a teapot|Misdirected Request|Unprocessable Entity|Locked|Failed Dependency|Too Early|Upgrade Required|Precondition Required|Too Many Requests|Request Header Fields Too Large|Unavailable For Legal Reasons|Internal Server Error|Not Implemented|Bad Gateway|Service Unavailable|Gateway Timeout|HTTP Version Not Supported|Variant Also Negotiates|Insufficient Storage|Loop Detected|Not Extended|Network Authentication Required)/,
+        response: ($) =>
+            seq($.http_version, WS, $.status_code, WS, $.status_text),
 
         request: ($) =>
             prec.right(seq(
@@ -108,13 +111,14 @@ module.exports = grammar({
             ),
 
         // {{foo}} {{$bar}} {{ fizzbuzz }}
-        variable: ($) => seq(
-            token(prec(1, "{{")),
-            optional(WS),
-            field("name", $.identifier),
-            optional(WS),
-            token(prec(1, "}}")),
-        ),
+        variable: ($) =>
+            seq(
+                token(prec(1, "{{")),
+                optional(WS),
+                field("name", $.identifier),
+                optional(WS),
+                token(prec(1, "}}")),
+            ),
 
         pre_request_script: ($) => seq("<", WS, $.script, NL),
         res_handler_script: ($) => seq(token(prec(1, ">")), WS, $.script, NL),
