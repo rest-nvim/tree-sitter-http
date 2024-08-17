f8856ddfe620d6a001bad39c9cb75f42f7afcbf7
Seongmin Lee <boltlessengineer@gmail.com>
2024-08-17 17:03:48 +0000

    refactor!: add raw-body type
    - drop support of form-body (can't distinguish from raw-body)
    - support multipart-form-data
    - fix several issues

--- grammar.js
+++ grammar.js
@@ -3,12 +3,16 @@ const PUNCTUATION = /[^\n\r\p{Z}\p{L}\p{N}]/u;
 const WS = /\p{Zs}+/u;
 const NL = token(choice("\n", "\r", "\r\n", "\0"));
 const LINE_TAIL = token(seq(/.*/, NL));
+const ESCAPED = token(/\\[^\n\r]/);
 
 module.exports = grammar({
     name: "http",
 
     extras: (_) => [],
-    conflicts: ($) => [[$.target_url]],
+    conflicts: ($) => [
+        [$.target_url],
+        [$._raw_body],
+    ],
     inline: ($) => [$._target_url_line],
 
     rules: {
@@ -23,16 +27,17 @@ module.exports = grammar({
         NL: (_) => NL,
         LINE_TAIL: (_) => LINE_TAIL,
 
+        _comment_prefix: (_) =>
+            choice(
+                token(prec(2, /#\s*/)),
+                token(prec(2, /\/\/\s*/)),
+            ),
         comment: ($) =>
             seq(
-                choice(
-                    repeat1(token(prec(1, "#"))),
-                    token(prec(1, seq("//", repeat("/")))),
-                ),
-                optional(token(prec(1, WS))),
+                $._comment_prefix,
                 choice(
                     seq(
-                        token(prec(1, "@")),
+                        token(prec(2, "@")),
                         field("name", $.identifier),
                         optional(
                             seq(
@@ -48,25 +53,10 @@ module.exports = grammar({
             ),
         request_separator: ($) =>
             seq(
-                token(prec(1, "###")),
+                token(prec(3, /###+\p{Zs}*/)),
                 optional(token(prec(1, WS))),
                 optional(field("value", $.value)),
                 NL,
-            ),
-        _comment_variable_declaration: ($) =>
-            seq(
-                optional(token(prec(1, WS))),
-                choice(
-                    seq(
-                        token(prec(1, "@")),
-                        field("name", $.identifier),
-                        optional(WS),
-                        "=",
-                        optional(token(prec(1, WS))),
-                        field("value", $.value),
-                    ),
-                    LINE_TAIL,
-                ),
             ),
 
         section: ($) =>
@@ -95,7 +85,7 @@ module.exports = grammar({
 
         // LIST http verb is arbitrary and required to use vaultproject
         method: (_) =>
-            /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH|LIST)/,
+            /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH|LIST|GRAPHQL|WEBSOCKET)/,
 
         http_version: (_) => token(prec(0, /HTTP\/[\d\.]+/)),
 
@@ -123,7 +113,7 @@ module.exports = grammar({
         status_text: (_) =>
             /(Continue|Switching Protocols|Processing|OK|Created|Accepted|Non-Authoritative Information|No Content|Reset Content|Partial Content|Multi-Status|Already Reported|IM Used|Multiple Choices|Moved Permanently|Found|See Other|Not Modified|Use Proxy|Switch Proxy|Temporary Redirect|Permanent Redirect|Bad Request|Unauthorized|Payment Required|Forbidden|Not Found|Method Not Allowed|Not Acceptable|Proxy Authentication Required|Request Timeout|Conflict|Gone|Length Required|Precondition Failed|Payload Too Large|URI Too Long|Unsupported Media Type|Range Not Satisfiable|Expectation Failed|I'm a teapot|Misdirected Request|Unprocessable Entity|Locked|Failed Dependency|Too Early|Upgrade Required|Precondition Required|Too Many Requests|Request Header Fields Too Large|Unavailable For Legal Reasons|Internal Server Error|Not Implemented|Bad Gateway|Service Unavailable|Gateway Timeout|HTTP Version Not Supported|Variant Also Negotiates|Insufficient Storage|Loop Detected|Not Extended|Network Authentication Required)/,
         response: ($) =>
-            seq($.http_version, WS, $.status_code, WS, $.status_text),
+            seq($.http_version, WS, $.status_code, WS, $.status_text, NL),
 
         request: ($) =>
             prec.right(seq(
@@ -131,21 +121,20 @@ module.exports = grammar({
                 field("url", $.target_url),
                 optional(seq(WS, field("version", $.http_version))),
                 NL, repeat($.comment),
-                optional(seq($.response, NL, repeat($.comment))),
+                optional($.response),
                 repeat(field("header", $.header)),
-                optional(seq(repeat1($._blank_line), repeat($.comment))),
                 optional(
-                    field(
-                        "body",
-                        choice(
-                            $.form_data,
-                            $.external_body,
-                            $.xml_body,
-                            $.json_body,
-                            $.graphql_data,
-                            $.multipart_form_data,
-                            // TODO: raw body
-                            // $.raw_body,
+                    seq(
+                        repeat1($._blank_line),
+                        repeat($.comment),
+                        optional(
+                            field("body", choice(
+                                $.raw_body,
+                                $.multipart_form_data,
+                                $.xml_body,
+                                $.json_body,
+                                $.graphql_body,
+                            ))
                         ),
                     ),
                 ),
@@ -188,8 +177,8 @@ module.exports = grammar({
                 token(prec(1, "}}")),
             ),
 
-        pre_request_script: ($) => seq("<", WS, $.script, NL),
-        res_handler_script: ($) => seq(token(prec(1, ">")), WS, $.script, NL),
+        pre_request_script: ($) => seq("<", WS, choice($.script, $.path), NL),
+        res_handler_script: ($) => seq(token(prec(3, ">")), WS, choice($.script, $.path), NL),
         script: (_) =>
             seq(
                 token(prec(1, "{%")),
@@ -211,62 +200,68 @@ module.exports = grammar({
 
         xml_body: (_) =>
             seq(
-                token(prec(1, /<\S/)),
+                token(prec(2, /<[^\s@]/)),
                 repeat1(LINE_TAIL),
             ),
 
         json_body: (_) =>
             seq(
-                token(prec(1, choice("{", "["))),
+                token(prec(2, /[{\[]\s+/)),
                 repeat1(LINE_TAIL),
             ),
 
-        graphql_data: ($) => seq($.graphql_body, optional($.json_body)),
-        graphql_body: (_) =>
+        graphql_body: ($) => seq($.graphql_data, optional($.json_body)),
+        graphql_data: (_) =>
             seq(
-                choice("query", "mutation"),
-                WS,
+                token(prec(2, seq(choice("query", "mutation"), WS, /.*\{/, NL))),
                 repeat1(LINE_TAIL),
             ),
 
         external_body: ($) =>
             seq(
-                token(prec(1, "<")),
+                token(prec(2, "<")),
                 optional(seq("@", field("name", $.identifier))),
                 WS,
-                field("path", $.value),
-                NL,
-            ),
-
-        form_data: ($) =>
-            seq(
-                $.query_param,
-                repeat(seq(optional(NL), token(prec(1, "&")), $.query_param)),
-                NL,
+                field("path", $.path),
             ),
 
         multipart_form_data: ($) =>
             prec.right(seq(
-                token(prec(1, "--")),
-                choice(
-                    LINE_TAIL,
-                ),
+                token(prec(2, "--")),
+                LINE_TAIL,
                 repeat(
                     choice(
                         $._blank_line,
                         $.comment,
-                        // TODO: `external_body` should end before `LINE_TAIL`
-                        $.external_body,
-                        LINE_TAIL,
+                        seq($.external_body, choice(WS, NL)),
+                        token(prec(1, LINE_TAIL)),
                     ),
                 ),
             )),
 
+        raw_body: ($) => $._raw_body,
+        _raw_body: ($) =>
+            seq(
+                choice(
+                    token(prec(1, LINE_TAIL)),
+                    seq($.external_body, NL),
+                    seq($._comment_prefix, $._not_comment),
+                ),
+                optional($._raw_body),
+            ),
+        _not_comment: (_) => token(seq(/[^@]*/, NL)),
+
         header_entity: (_) => /[\w\-]+/,
         identifier: (_) => /[A-Za-z_.\$\d\u00A1-\uFFFF-]+/,
-        number: (_) => /[0-9]+/,
-        string: (_) => /"[^"\n\r]*"/,
-        boolean: (_) => choice("true", "false"),
+        path: ($) =>
+            prec.right(repeat1(
+                choice(
+                    WORD_CHAR,
+                    PUNCTUATION,
+                    $.variable,
+                    ESCAPED,
+                )
+            )),
         value: ($) =>
             repeat1(
                 choice(
