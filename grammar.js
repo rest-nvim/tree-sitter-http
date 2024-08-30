98c4d2f43141e6ed8b044875cb73d63e10b70301
boltlessengineer <boltlessengineer@users.noreply.github.com>
2024-08-30 08:02:26 +0000

    chore: autoformat with dprint

--- grammar.js
+++ grammar.js
@@ -128,7 +128,8 @@ module.exports = grammar({
         status_text: (_) =>
             /(Continue|Switching Protocols|Processing|OK|Created|Accepted|Non-Authoritative Information|No Content|Reset Content|Partial Content|Multi-Status|Already Reported|IM Used|Multiple Choices|Moved Permanently|Found|See Other|Not Modified|Use Proxy|Switch Proxy|Temporary Redirect|Permanent Redirect|Bad Request|Unauthorized|Payment Required|Forbidden|Not Found|Method Not Allowed|Not Acceptable|Proxy Authentication Required|Request Timeout|Conflict|Gone|Length Required|Precondition Failed|Payload Too Large|URI Too Long|Unsupported Media Type|Range Not Satisfiable|Expectation Failed|I'm a teapot|Misdirected Request|Unprocessable Entity|Locked|Failed Dependency|Too Early|Upgrade Required|Precondition Required|Too Many Requests|Request Header Fields Too Large|Unavailable For Legal Reasons|Internal Server Error|Not Implemented|Bad Gateway|Service Unavailable|Gateway Timeout|HTTP Version Not Supported|Variant Also Negotiates|Insufficient Storage|Loop Detected|Not Extended|Network Authentication Required)/,
         response: ($) =>
-            seq($.http_version, WS, $.status_code, WS, optional($.status_text), NL),
+            seq($.http_version, WS, $.status_code, WS, optional($.status_text),
+                NL),
 
         request: ($) =>
             prec.right(seq(
